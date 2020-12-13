2c986574687a8b3358a8c33c47cfbdfa583e19c9
Ricardo Casilimas <rcasilimas@gmail.com>
2020-12-13 15:01:16 -0500

    fixing faulty parent pics, added placeholder

--- src/components/ParentCard/ParentCard.js
+++ src/components/ParentCard/ParentCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import placeholder from '../../img/noimage.png'
 import "./ParentCard.scss";
 
 const ParentCard = ({picture, name, title}) => {
+
+    const addDefaultSrc = (ev) => {
+        ev.target.src = placeholder;
+        console.log(ev.target)
+      }
 
 
     return (
         <div className="parent-container">
             <h3>{title}</h3>
-            <img src={picture} alt="parent" className="parent-pic" />
+            <img onError={addDefaultSrc} src={picture} alt="parent" className="parent-pic" />
             <h3>{name}</h3>
         </div>
     )
